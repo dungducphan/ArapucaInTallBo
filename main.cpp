ac8e45969fd8718dbd1d069d32327ede79e8b92a
Dung Phan <brianp.dung@utexas.edu>
2017-10-03 15:03:05 -0500

    Adding FFT filter. Changing main to test FFT filter.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 
 #include <TGraph.h>
@@ -40,4 +41,18 @@ int main() {
     c->SaveAs("testFilter.png");
 
     return 0;
+}
+*/
+
+#include "ROOTUtilities/DataAccess.h"
+#include <fftw3.h>
+{
+fftw_complex *in, *out;
+fftw_plan p;
+in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+fftw_execute(p); /* repeat as needed */
+fftw_destroy_plan(p);
+fftw_free(in); fftw_free(out);
 }
