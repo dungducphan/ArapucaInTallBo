ca859d4db859cfb8e6cc5f9f759f72639fffaf8b
Dung Phan <brianp.dung@utexas.edu>
2017-10-03 15:01:43 -0500

    Adding FFT filter.

---
+++ FilterUtilities/FFTCutoff.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Dung Phan on 10/3/17.
+//
+
+#include "FFTCutoff.h"
+
+FFTCutoff::FFTCutoff(unsigned int sampleIdx) {
+    kNumberOfSamplingPoints = sampleIdx;
+}
+
+FFTCutoff::~FFTCutoff() {
+}
