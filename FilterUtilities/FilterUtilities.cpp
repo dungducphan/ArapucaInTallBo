5ac422c3434419d3641899058671b1026f9f6f8d
Dung Phan <brianp.dung@utexas.edu>
2017-10-03 17:39:12 -0500

    Fully functional FFT filter.

--- FilterUtilities/FilterUtilities.cpp
+++ FilterUtilities/FilterUtilities.cpp
@@ -14,21 +14,24 @@ void FilterUtilities::AddGolaySavitzkyFilter(unsigned int NumberOfPoints, unsign
     FilterSequence.push_back(kGS);
 }
 
-void FilterUtilities::Filter(double *waveform, double *filteredWaveform, unsigned int sampleIdx) {
+void FilterUtilities::Filter(double *waveform, double *filteredWaveform) {
     unsigned int GSFilterIdx = 0;
     unsigned int FFTFilterIdx = 0;
-    intermediateInput   = (double*)malloc(sampleIdx * sizeof(double));
-    intermediateOutput  = (double*)malloc(sampleIdx * sizeof(double));
-    std::copy(waveform, waveform + sampleIdx, intermediateInput);
+    intermediateInput = (double *) malloc(kNumberOfSamplingPoints * sizeof(double));
+    intermediateOutput = (double *) malloc(kNumberOfSamplingPoints * sizeof(double));
+    std::copy(waveform, waveform + kNumberOfSamplingPoints, intermediateInput);
     for (int i = 0; i < FilterSequence.size(); i++) {
         switch (FilterSequence.at(i)) {
             case kGS: {
-                GSFilterCollection.at(GSFilterIdx)->Filter(intermediateInput, intermediateOutput, sampleIdx);
-                std::copy(intermediateOutput, intermediateOutput + sampleIdx, intermediateInput);
+                GSFilterCollection.at(GSFilterIdx)->Filter(intermediateInput, intermediateOutput,
+                                                           kNumberOfSamplingPoints);
+                std::copy(intermediateOutput, intermediateOutput + kNumberOfSamplingPoints, intermediateInput);
                 GSFilterIdx++;
                 break;
             }
             case kFFT: {
+                FFTCutoffCollection.at(FFTFilterIdx)->Filter(intermediateInput, intermediateOutput);
+                std::copy(intermediateOutput, intermediateOutput + kNumberOfSamplingPoints, intermediateInput);
                 FFTFilterIdx++;
                 break;
             }
@@ -39,7 +42,25 @@ void FilterUtilities::Filter(double *waveform, double *filteredWaveform, unsigne
         }
     }
 
-    std::copy(intermediateOutput, intermediateOutput + sampleIdx, filteredWaveform);
+    std::copy(intermediateOutput, intermediateOutput + kNumberOfSamplingPoints, filteredWaveform);
+}
+
+void FilterUtilities::AddFFTCutoffFilter(FreqRange cutRange) {
+    FFTCutoff *fftCutoff = new FFTCutoff(kNumberOfSamplingPoints);
+    fftCutoff->SetFreqCutoffRange(cutRange);
+    FFTCutoffCollection.push_back(fftCutoff);
+    FilterSequence.push_back(kFFT);
+}
+
+void FilterUtilities::SetNumberOfSamplingPoints(unsigned int numberOfSamplingPoints) {
+    kNumberOfSamplingPoints = numberOfSamplingPoints;
+}
+
+void FilterUtilities::AddFFTCutoffFilterByValue(unsigned int freqCutValue) {
+    FFTCutoff *fftCutoff = new FFTCutoff(kNumberOfSamplingPoints);
+    fftCutoff->SetFreqCutoffValue(freqCutValue);
+    FFTCutoffCollection.push_back(fftCutoff);
+    FilterSequence.push_back(kFFT);
 }
 
 
--- FilterUtilities/FilterUtilities.h
+++ FilterUtilities/FilterUtilities.h
@@ -6,6 +6,7 @@
 #define TALLBOANALYSIS_FILTERUTILITIES_H
 
 #include "GolaySavitzkyCoeff.h"
+#include "FFTCutoff.h"
 
 enum FilterTypes {kGS, kFFT};
 
@@ -15,13 +16,23 @@ public:
     virtual ~FilterUtilities();
 
     virtual void AddGolaySavitzkyFilter(unsigned int NumberOfPoints, unsigned int Degree);
-    virtual void Filter(double* waveform, double* filteredWaveform, unsigned int sampleIdx);
+
+    virtual void AddFFTCutoffFilter(FreqRange cutRange);
+
+    virtual void AddFFTCutoffFilterByValue(unsigned int freqCutValue);
+
+    virtual void Filter(double *waveform, double *filteredWaveform);
+
+    virtual void SetNumberOfSamplingPoints(unsigned int numberOfSamplingPoints);
 
 private:
     double* intermediateInput;
     double* intermediateOutput;
     std::vector<GolaySavitzkyCoeff*> GSFilterCollection;
+    std::vector<FFTCutoff *> FFTCutoffCollection;
     std::vector<FilterTypes> FilterSequence;
+
+    unsigned int kNumberOfSamplingPoints;
 };
 
 #endif //TALLBOANALYSIS_FILTERUTILITIES_H
