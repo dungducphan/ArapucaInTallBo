5e536e12559645a3d47b47f16068ffa40bd717cf
Dung Phan <brianp.dung@utexas.edu>
2017-10-03 18:22:26 -0500

    Adding inline documentation.

--- FilterUtilities/FilterUtilities.cpp
+++ FilterUtilities/FilterUtilities.cpp
@@ -9,17 +9,65 @@ FilterUtilities::FilterUtilities() {
 
 FilterUtilities::~FilterUtilities() {}
 
+void FilterUtilities::SetNumberOfSamplingPoints(unsigned int numberOfSamplingPoints) {
+    kNumberOfSamplingPoints = numberOfSamplingPoints;
+}
+
 void FilterUtilities::AddGolaySavitzkyFilter(unsigned int NumberOfPoints, unsigned int Degree) {
+    /*
+     * Create and push a new pointer, pointing to GolaySavitzkyCoeff object,
+     * into the GSFilterCollection.
+     *
+     * Push a kGS value into the FilterSequence to notify a GSFilter to be
+     * performed.
+     */
     GSFilterCollection.push_back(new GolaySavitzkyCoeff(NumberOfPoints, Degree));
     FilterSequence.push_back(kGS);
+}
+
+void FilterUtilities::AddFFTCutoffFilter(FreqRange cutRange) {
+    /*
+     * Create a pointer of FFTCutoff. Specify frequency cut by range.
+     * See FilterUtilities.h or FFTCutoff.h for more details.
+     *
+     * Push the new pointer, pointing to FFTCutoff object,
+     * into the FFTCutoffCollection.
+     *
+     * Push a kFFT value into the FilterSequence to notify a FFTfilter
+     * to be performed.
+     */
+    FFTCutoff *fftCutoff = new FFTCutoff(kNumberOfSamplingPoints);
+    fftCutoff->SetFreqCutoffRange(cutRange);
+    FFTCutoffCollection.push_back(fftCutoff);
+    FilterSequence.push_back(kFFT);
+}
+
+
+void FilterUtilities::AddFFTCutoffFilterByValue(unsigned int freqCutValue) {
+    /*
+     * Create a pointer of FFTCutoff. Specify frequency cut by value.
+     * See FilterUtilities.h or FFTCutoff.h for more details.
+     *
+     * Push the new pointer, pointing to FFTCutoff object,
+     * into the FFTCutoffCollection.
+     *
+     * Push a kFFT value into the FilterSequence to notify a FFTfilter
+     * to be performed.
+     */
+    FFTCutoff *fftCutoff = new FFTCutoff(kNumberOfSamplingPoints);
+    fftCutoff->SetFreqCutoffValue(freqCutValue);
+    FFTCutoffCollection.push_back(fftCutoff);
+    FilterSequence.push_back(kFFT);
 }
 
 void FilterUtilities::Filter(double *waveform, double *filteredWaveform) {
     unsigned int GSFilterIdx = 0;
     unsigned int FFTFilterIdx = 0;
+
     intermediateInput = (double *) malloc(kNumberOfSamplingPoints * sizeof(double));
     intermediateOutput = (double *) malloc(kNumberOfSamplingPoints * sizeof(double));
     std::copy(waveform, waveform + kNumberOfSamplingPoints, intermediateInput);
+
     for (int i = 0; i < FilterSequence.size(); i++) {
         switch (FilterSequence.at(i)) {
             case kGS: {
@@ -45,23 +93,7 @@ void FilterUtilities::Filter(double *waveform, double *filteredWaveform) {
     std::copy(intermediateOutput, intermediateOutput + kNumberOfSamplingPoints, filteredWaveform);
 }
 
-void FilterUtilities::AddFFTCutoffFilter(FreqRange cutRange) {
-    FFTCutoff *fftCutoff = new FFTCutoff(kNumberOfSamplingPoints);
-    fftCutoff->SetFreqCutoffRange(cutRange);
-    FFTCutoffCollection.push_back(fftCutoff);
-    FilterSequence.push_back(kFFT);
-}
 
-void FilterUtilities::SetNumberOfSamplingPoints(unsigned int numberOfSamplingPoints) {
-    kNumberOfSamplingPoints = numberOfSamplingPoints;
-}
-
-void FilterUtilities::AddFFTCutoffFilterByValue(unsigned int freqCutValue) {
-    FFTCutoff *fftCutoff = new FFTCutoff(kNumberOfSamplingPoints);
-    fftCutoff->SetFreqCutoffValue(freqCutValue);
-    FFTCutoffCollection.push_back(fftCutoff);
-    FilterSequence.push_back(kFFT);
-}
 
 
 
--- FilterUtilities/FilterUtilities.h
+++ FilterUtilities/FilterUtilities.h
@@ -8,22 +8,101 @@
 #include "GolaySavitzkyCoeff.h"
 #include "FFTCutoff.h"
 
+/*
+ * This enum specifies the type of filters user want to perform
+ *      - kGS   : Golay-Savitzky filter.
+ *      - kFFT  : FFT filter.
+ */
 enum FilterTypes {kGS, kFFT};
 
 class FilterUtilities {
 public:
+    /*
+     * This class is a wrapper that allows user to choose
+     * to perform different types of filters on the data,
+     * as well as repeat a specific filter many times.
+     *
+     * It provides methods to create, configure and add
+     * wanted filters into the filter sequence to be performed.
+     *
+     * HOW TO USE:
+     *      - Create a FilterUtilities object.
+     *      - Set number of sampling points N of the data.
+     *      - Add wanted filters method with their parameters. See their classes for details.
+     *      - Call Filter(input, output) function to perform filter sequence.
+     *      - Notice that input and output have to be double array with N elements.
+     *
+     * Example usage:
+     *
+     *      FilterUtilities* myFilter = new FilterUtilities();
+     *      myFilter->SetNumberOfSamplingPoints(1650);
+     *      myFilter->AddGolaySavitzkyFilter(5, 3);
+     *      myFilter->AddFFTCutoffFilterByValue(800);
+     *      myFilter->AddGolaySavitzkyFilter(25, 5);
+     *      myFilter->Filter(adc, filteredadc);
+     */
     FilterUtilities();
     virtual ~FilterUtilities();
 
+    /*
+     * Set the number of sampling points in a waveform.
+     *
+     * For e.g.: If waveform contains of 1024 ADC values, then we call:
+     *
+     *      FilterUtilities* myFilter = new FilterUtilities();
+     *      myFilter->SetNumberOfSamplingPoints(1024);
+     */
+    virtual void SetNumberOfSamplingPoints(unsigned int numberOfSamplingPoints);
+
+    /*
+     * IMPORTANT NOTICE:
+     * User needs to call the Add...Filter() methods in the order in which
+     * they want the filters are perform.
+     *
+     * For e.g.: To perform a sequence of Golay-Savitzky filter, then
+     *           FFT filter and then another Golay-Savitzky, we call:
+     *
+     *      FilterUtilities* myFilter = new FilterUtilities();
+     *      myFilter->SetNumberOfSamplingPoints(1650);
+     *      myFilter->AddGolaySavitzkyFilter(5, 3);
+     *      myFilter->AddFFTCutoffFilterByValue(800);
+     *      myFilter->AddGolaySavitzkyFilter(25, 5);
+     *
+     * The order of filters to be performed is then saved in FilterSequence.
+     */
+
+    /*
+     * Adding GolaySavitzkyFilter object
+     * Need to specify number of points in a filter window and the degree of the polynomial filter
+     */
     virtual void AddGolaySavitzkyFilter(unsigned int NumberOfPoints, unsigned int Degree);
 
+    /*
+     * Adding GolaySavitzkyFilter object
+     * Need to specify the cutoff frequency
+     * Cutoff range is pre-defined in the FFTCutoff class
+     *      - kMedium   : cutoff starts at 1/4 of the waveform.
+     *      - kHigh     : cutoff starts at 1/2 of the waveform.
+     *      - kVeryHigh : cutoff starts at 3/4 of the waveform.
+     */
     virtual void AddFFTCutoffFilter(FreqRange cutRange);
 
+    /*
+     * Adding GolaySavitzkyFilter object
+     * Need to specify the cutoff frequency
+     * Cutoff freq is an index number between 0 and kNumberOfSamplingPoints
+     */
     virtual void AddFFTCutoffFilterByValue(unsigned int freqCutValue);
 
+    /*
+     * Perform filtering on data
+     *      - waveform          : input waveform.
+     *      - filteredWaveform  : filtered output waveform.
+     *
+     * Filter() calls different filtering methods in an user-defined order specified
+     * by FilterSequence.
+     */
     virtual void Filter(double *waveform, double *filteredWaveform);
-
-    virtual void SetNumberOfSamplingPoints(unsigned int numberOfSamplingPoints);
 
 private:
     double* intermediateInput;
@@ -32,6 +111,9 @@ private:
     std::vector<FFTCutoff *> FFTCutoffCollection;
     std::vector<FilterTypes> FilterSequence;
 
+    /*
+     * Number of sampling points of a waveform.
+     */
     unsigned int kNumberOfSamplingPoints;
 };
 
